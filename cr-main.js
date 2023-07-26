fa4098f0f39e9305b52f26ed50bb1acb9366f4af
dhuttyy-vygo <45688413+dhuttyy-vygo@users.noreply.github.com>
2023-07-26 11:06:29 +0100

    Update cr-main.js

--- cr-main.js
+++ cr-main.js
@@ -289,3 +289,44 @@ $(".projects_link").each(function (index) {
       .fromTo($(this).find(".projects_img"), { backgroundPosition: "50% 100%" }, { backgroundPosition: "50% 0%" })
       .fromTo($(this).find(".projects_content-wrap"), { yPercent: 50 }, { yPercent: -50 }, 0);
   });
+
+  $("[js-vimeo-element='component']").each(function (index) {
+    let componentEl = $(this),
+      coverEl = $(this).find("[js-vimeo-element='cover']");
+  
+    let relatedPopupVideo = $(".popup_item").eq(index);
+    let iframeEl = relatedPopupVideo.find("iframe");
+    let closePop = $(".cccc.is-popup");
+  
+    iframeEl.attr("preload", "auto"); // Set the preload attribute to "auto"
+  
+    let player = new Vimeo.Player(iframeEl[0]);
+  
+    player.on('bufferstart', function () {
+      let playingCover = $("[js-vimeo-element='component'].is-playing").not(componentEl).find(" [js-vimeo-element='cover']");
+      if (playingCover.length) playingCover[0].click();
+      componentEl.addClass("is-playing");
+    });
+  
+    player.on('play', function () {
+      let playingCover = $("[js-vimeo-element='component'].is-playing").not(componentEl).find(" [js-vimeo-element='cover']");
+      if (playingCover.length) playingCover[0].click();
+      componentEl.addClass("is-playing");
+    });
+  
+    player.on("pause", function () {
+      componentEl.removeClass("is-playing");
+    });
+  
+    closePop.on("click", function () {
+      player.pause();
+    });
+  
+    coverEl.on("click", function () {
+      if (componentEl.hasClass("is-playing")) {
+        player.pause();
+      } else {
+        player.play();
+      }
+    });
+  });
